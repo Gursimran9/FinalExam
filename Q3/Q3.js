4718adc20d8057026904359410c9bef73893ee6a
Gursimran9 <55466920+Gursimran9@users.noreply.github.com>
2020-04-16 13:30:01 -0700

    Update Q3.js

--- Q3/Q3.js
+++ Q3/Q3.js
@@ -11,19 +11,19 @@ function getCats(){
   .then((response) => response.json())  //
   .then((data) => {         //get data
     let display = '<h2 class="Cats">Cats</h2>';//created a display var that will display the data
-    data.forEach(function(Cats){
+    data.forEach(function(cats){
      
 //Output the data of Catss.json file i.e. image,name,price and details
 //Used card template from bootstrap to display data in Cards
       display +=`
       <div class="card" style="width: 18rem;">
   <div class="card-body">
-  <img class="card-img-top" src=${Cats.image} alt="Card image cap"> 
-    <h5 class="card-title">Name: ${Cats.name}</h5>
+  <img class="card-img-top" src=${cats.image} alt="Card image cap"> 
+    <h5 class="card-title">Name: ${cats.name}</h5>
     <hr>
-    <h5 class="card-subtitle mb-2 text-muted">Price: ${Cats.species}</h6>
+    <h5 class="card-subtitle mb-2 text-muted">Price: ${cats.species}</h6>
     <hr co>
-    <h5 class="card-subtitle mb-2 text-muted">Details: ${Cats.age}</h6>
+    <h5 class="card-subtitle mb-2 text-muted">Details: ${cats.age}</h6>
   </div>
 </div>
     `;
